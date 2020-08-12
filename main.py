ea9a5b566516af37f2423955a6103539093d3e62
guilhermenakahata <guiilhermehsouza@gmail.com>
2020-08-11 21:36:41 -0300

    [11-08-2020] Cria matriz de confusão bonitona.

--- main.py
+++ main.py
@@ -507,7 +507,7 @@ def PlotarGraficoTodosKfolds():
 #- Monta matriz de confusao -
 #----------------------------
 
-def montarMatriz():
+def montarMatriz(titleMatrix):
 
     validacaoAccuracy = []
 
@@ -562,7 +562,7 @@ def montarMatriz():
         y_val_stringGlobal = pd.concat([y_val_stringGlobal, y_val_string])
         predGlobal = pd.concat([predGlobal, pred])
 
-        loss1, accuracy1 = model.evaluate(X_val, y_val, steps = 20)
+        loss1, accuracy1 = model.evaluate(X_val, y_val, steps = 18)
 
         validacaoAccuracy.append(accuracy1)
 
@@ -592,18 +592,63 @@ def montarMatriz():
     f1 = metrics.f1_score(predGlobal, y_val_stringGlobal, average='macro')
     print('F1 score: %f' % f1)
 
-    df_cm = pd.DataFrame(confusion_matrix, index=['Formal_Upright','Wind_Swept','Semi_Cascade','Cascade','Literati','Informal_Upright','Slanting'] , columns=['Formal_Upright','Wind_Swept','Semi_Cascade','Cascade','Literati','Informal_Upright','Slanting'])
-    sn.set(font_scale=1) # for label size
-    sn.heatmap(df_cm/np.sum(df_cm), annot=True, annot_kws={"size": 12}, cmap='Greens') # font size
+    plot_confusion_matrix(confusion_matrix,Style,titleMatrix,"Greens")
 
+#------------------------------
+#- Plota a matriz de confusao -
+#------------------------------
+
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import itertools
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.2f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
     plt.show()
+
 
 #---------------------------
 #- Começa aqui o algoritmo -
 #---------------------------
 
 # PlotarGraficoTodosKfolds()
-# montarMatriz()
+montarMatriz("VGG16")
 
 # inceptionv3 = keras.models.load_model('GoogleNet10EpochsK-Folds1.tf')
 # print("Modelo carregado!")
